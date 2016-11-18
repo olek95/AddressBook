97886d305a294338b88fb67f8ca40b946534be9d
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-18 14:13:16 +0100

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,21 @@
+package addressbook;
+
+public class Person implements Comparable<Person>{
+    private String name, lastName, email;
+    public Person(String name, String lastName, String email){
+        this.name = name;
+        this.lastName = lastName; 
+        this.email = email; 
+    }
+    public int compareTo(Person p){
+        if(name.compareTo(p.name) < 0)return -1;
+        else if(name.compareTo(p.name) > 0) return 1; 
+        else if(name.compareTo(p.name) == 0)
+            if(lastName.compareTo(p.lastName) < 0) return -1;
+            else if(lastName.compareTo(p.lastName) > 0) return 1; 
+            else if(lastName.compareTo(p.lastName) == 0)
+                if(email.compareTo(p.email) < 0) return -1; 
+                else if(email.compareTo(p.email) > 0) return 1;
+        return 0;
+    }
+}
